b6a88ea164294fde1dc5e6197edbbd10af34c556
Maksim <64214493+MaxGlinko78@users.noreply.github.com>
2022-02-12 23:54:15 +0300

    new card

--- js/main.js
+++ js/main.js
@@ -13,3 +13,44 @@ for (let i = 0; i < arrow.length; i++) {
 menuBtn.addEventListener("click", () => {
   sidebar.classList.toggle("close");
 });
+
+
+const getButton = document.querySelectorAll('.get-booking');
+const modal = document.querySelector('.modal');
+const ModalClose = document.querySelector('.header-modal__close');
+
+
+
+
+getButton.forEach(function(item){
+	
+	item.addEventListener('click', () => {
+    modal.classList.toggle('active');     
+  });
+	ModalClose.addEventListener('click', () => {
+	modal.classList.remove('active');
+		});
+	
+});
+
+const linkTo = document.querySelectorAll(".link-to");
+const card = document.querySelector('.card');
+
+const active = document.querySelector('.active');
+
+		
+linkTo.forEach(function(item){
+	item.addEventListener('click', () => {
+		
+		card.classList.toggle('active');
+	     
+	 });
+
+		
+});
+
+
+
+
+
+
